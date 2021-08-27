518c8893b203177c0d7b3c173adb6bde59675e00
cabbit <perasutlife@gmail.com>
2021-08-27 14:33:44 +0700

    login API

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+let brand = " | Inventory Vue SPA"
+export default {
+  name:'App',
+  watch:{
 
-#nav {
-  padding: 30px;
+    $route(to){
+      document.title = to.meta.title + brand,
+      document.description = to.meta.description
+  }
+  }
 }
+</script>
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,155 @@
+
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+
+// Import Layouts
+import FrontendLayout from '@/layouts/Frontend.vue'
+
+// Import Views
+// Frontend
+import Home from '@/views/frontend/Home.vue'
+import About from '@/views/frontend/About.vue'
+import Portfolio from '@/views/frontend/Portfolio.vue'
+import Service from '@/views/frontend/Service.vue'
+import Contact from '@/views/frontend/Contact.vue'
+import Register from '@/views/frontend/Register.vue'
+import Login from '@/views/frontend/Login.vue'
+import ForgotPassword from '@/views/frontend/ForgotPassword.vue'
+import NotFound404 from '@/views/frontend/NotFound404.vue'
+
+// Backend
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Home
+      }
+    ],
+    meta: {
+      title: 'หน้าหลัก',
+      description: 'หน้าหลักระบบคงคลังสินค้า'
+    }
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: About
+      }
+    ],
+    meta: {
+      title: 'เกี่ยวกับเรา',
+      description: 'รายละเอียดหน้าเกี่ยวกับเรา'
+    }
+  },
+  {
+    path: '/portfolio',
+    name: 'Portfolio',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Portfolio
+      }
+    ],
+    meta: {
+      title: 'ผลงานของเรา',
+      description: 'รายละเอียดหน้าผลงานของเรา'
+    }
+  },
+  {
+    path: '/service',
+    name: 'Service',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Service
+      }
+    ],
+    meta: {
+      title: 'บริการของเรา',
+      description: 'รายละเอียดหน้าบริการของเรา'
+    }
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Contact
+      }
+    ],
+    meta: {
+      title: 'ติดต่อเรา',
+      description: 'รายละเอียดหน้าติดต่อเรา'
+    }
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Register
+      }
+    ],
+    meta: {
+      title: 'สมัครสมาชิกใหม่',
+      description: 'รายละเอียดสมัครสมาชิกใหม่'
+    }
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Login
+      }
+    ],
+    meta: {
+      title: 'เข้าสู่ระบบ',
+      description: 'รายละเอียดหน้าเข้าสู่ระบบ'
+    }
+  },
+  {
+    path: '/forgotpassword',
+    name: 'ForgotPassword',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: ForgotPassword
+      }
+    ],
+    meta: {
+      title: 'ลืมรหัสผ่าน',
+      description: 'รายละเอียดหน้าลืมรหัสผ่าน'
+    }
+  },
+
+  // Error 404 
+  {
+    path: "/:catchAll(.*)",
+    component: NotFound404,
+    meta: {
+      title: '404 ไม่พบหน้านี้',
+      description: 'รายละเอียดหน้า 404'
+    }
   }
+
 ]
 
 const router = createRouter({
